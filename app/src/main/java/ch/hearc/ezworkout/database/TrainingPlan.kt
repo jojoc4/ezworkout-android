9d8e90d113c6c720683442284cafdebc85a042bf
massimo.desantis <massimo.desantis@he-arc.ch>
2020-10-15 11:31:00 +0200

    rip?

--- app/src/main/java/ch/hearc/ezworkout/database/TrainingPlan.kt
+++
@@ -1,8 +0,0 @@
-package ch.hearc.ezworkout.database
-@Entity
-data class TrainingPlan (
-    @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "first_name") val firstName: String?,
-    @ColumnInfo(name = "last_name") val lastName: String?
-
-)
--- app/src/main/java/ch/hearc/ezworkout/database/db.kt
+++
@@ -1,7 +0,0 @@
-package ch.hearc.ezworkout.database
-
-@Database(entities = arrayOf(User::class), version = 1)
-abstract class db : RoomDatabase() {
-
-
-}
