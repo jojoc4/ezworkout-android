b7f29214136f588d11478647255de35340e48acb
jonatan baumgartner <jonatan.baumgartner@he-arc.ch>
2020-10-15 10:31:42 +0200

    Sync fragment

---
+++ app/src/main/java/ch/hearc/ezworkout/ui/sync/SyncFragment.kt
@@ -0,0 +1,33 @@
+package ch.hearc.ezworkout.ui.sync
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import ch.hearc.ezworkout.R
+import ch.hearc.ezworkout.ui.notifications.NotificationsViewModel
+
+class SyncFragment : Fragment() {
+
+    private lateinit var syncViewModel: SyncViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        syncViewModel =
+            ViewModelProviders.of(this).get(SyncViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_sync, container, false)
+        val textView: TextView = root.findViewById(R.id.text_sync)
+        syncViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+}
