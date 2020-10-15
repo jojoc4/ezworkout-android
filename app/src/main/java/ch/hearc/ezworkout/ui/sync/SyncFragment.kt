2f88fa4879c1094085548cb55c4548c92c710dfb
jonatan baumgartner <jonatan.baumgartner@he-arc.ch>
2020-10-15 12:01:23 +0200

    QRScanner ok

--- app/src/main/java/ch/hearc/ezworkout/ui/sync/SyncFragment.kt
+++ app/src/main/java/ch/hearc/ezworkout/ui/sync/SyncFragment.kt
@@ -1,11 +1,13 @@
 package ch.hearc.ezworkout.ui.sync
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import ch.hearc.ezworkout.R
@@ -27,7 +29,16 @@ class SyncFragment : Fragment() {
         syncViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val btn: Button = root.findViewById(R.id.scanner)
+        btn.setOnClickListener{
+            val intent = Intent(activity, QRReader::class.java)
+            startActivity(intent)
+        }
+
         return root
     }
+
+
 
 }
