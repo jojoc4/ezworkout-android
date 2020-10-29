417b6d89799bc66d301484732ab5859108836799
louis.bandelie <louis.bandelier@he-arc.ch>
2020-10-29 11:19:25 +0100

    Implemented 2 bundle test models

--- app/src/main/java/ch/hearc/ezworkout/ui/activities/trainings/ATrainings.kt
+++ app/src/main/java/ch/hearc/ezworkout/ui/activities/trainings/ATrainings.kt
@@ -1,12 +1,17 @@
 package ch.hearc.ezworkout.ui.activities.trainings
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import ch.hearc.ezworkout.MainActivity
 import ch.hearc.ezworkout.R
+
 
 class ATrainings : Fragment() {
 
@@ -20,7 +25,29 @@ class ATrainings : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.a_trainings_fragment, container, false)
+        // METHOD 1 TO PASS VALUES WITHIN THE SAME ACTIVITY
+        val activity: MainActivity? = activity as MainActivity
+        val mainMyString: String = activity?.getMyString() ?: "Error"
+        Log.d("Var from activity", mainMyString)
+
+        // METHOD 2 TO PASS VALUES TO ANOTHER ACTIVITY
+        val root = inflater.inflate(R.layout.fragment_sync, container, false)
+        val buttonStart: Button = root.findViewById(R.id.start)
+        buttonStart.setOnClickListener {
+            /* Create a new activity and pass the bundle to it
+            val intent = Intent(activity, SecondActivity::class.java)
+            val bundle = Bundle()
+            bundle.putString("key_1", "VARIABLE FROM BUNDLE")
+            intent.putExtras(bundle)
+            startActivity(intent)
+            */
+            /* To recover the info in the target "SecondActivity" class
+            Bundle bundle = getIntent().getExtras();
+            String title = bundle.getString("key_1");
+             */
+        }
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
