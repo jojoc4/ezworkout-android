25955044354ab73d299c57a869840cf801ac4e70
louis.bandelie <louis.bandelier@he-arc.ch>
2020-11-05 11:01:33 +0100

    Linked pages

--- app/src/main/java/ch/hearc/ezworkout/ui/activities/trainings/ATrainings.kt
+++ app/src/main/java/ch/hearc/ezworkout/ui/activities/trainings/ATrainings.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import ch.hearc.ezworkout.MainActivity
 import ch.hearc.ezworkout.R
+import ch.hearc.ezworkout.ui.activities.exercices.ExercicesActivity
 
 
 class ATrainings : Fragment() {
@@ -25,26 +26,22 @@ class ATrainings : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // METHOD 1 TO PASS VALUES WITHIN THE SAME ACTIVITY
+        // METHOD TO PASS VALUES WITHIN THE SAME ACTIVITY
+        /*
         val activity: MainActivity? = activity as MainActivity
         val mainMyString: String = activity?.getMyString() ?: "Error"
         Log.d("Var from activity", mainMyString)
+        */
 
-        // METHOD 2 TO PASS VALUES TO ANOTHER ACTIVITY
-        val root = inflater.inflate(R.layout.fragment_sync, container, false)
+        val root = inflater.inflate(R.layout.a_trainings_fragment, container, false)
         val buttonStart: Button = root.findViewById(R.id.start)
         buttonStart.setOnClickListener {
-            /* Create a new activity and pass the bundle to it
-            val intent = Intent(activity, SecondActivity::class.java)
+            // Create a new activity and pass the bundle to it
+            val intent = Intent(activity, ExercicesActivity::class.java)
             val bundle = Bundle()
-            bundle.putString("key_1", "VARIABLE FROM BUNDLE")
+            bundle.putString("training_id", "1")
             intent.putExtras(bundle)
             startActivity(intent)
-            */
-            /* To recover the info in the target "SecondActivity" class
-            Bundle bundle = getIntent().getExtras();
-            String title = bundle.getString("key_1");
-             */
         }
 
         return root
