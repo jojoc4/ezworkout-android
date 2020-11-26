15e94eb6df5ff6944bf30189cc659320c60478b6
jonatan baumgartner <jonatan.baumgartner@he-arc.ch>
2020-11-26 11:32:21 +0100

    json

--- app/src/main/java/ch/hearc/ezworkout/ui/sync/SynchronizeThread.kt
+++ app/src/main/java/ch/hearc/ezworkout/ui/sync/SynchronizeThread.kt
@@ -4,7 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.util.Log
 import androidx.preference.PreferenceManager
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
 class SynchronizeThread : Runnable {
@@ -18,5 +23,15 @@ class SynchronizeThread : Runnable {
 
     override fun run() {
         Log.i("SYNCHRONIZETHREAD", "thread run")
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.i("SYNCHRONIZETHREAD", "Response: %s".format(response.toString()))
+            },
+            Response.ErrorListener { error ->
+                Log.i("SYNCHRONIZETHREAD", "Erreur")
+            }
+        )
+        queue.add(jsonObjectRequest)
+
     }
 }
