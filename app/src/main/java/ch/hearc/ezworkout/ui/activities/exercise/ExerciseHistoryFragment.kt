9292fba9d62ccfc906d74546924537df49bc837c
louis.bandelie <louis.bandelier@he-arc.ch>
2021-01-21 12:01:23 +0100

    Updated

--- app/src/main/java/ch/hearc/ezworkout/ui/activities/exercise/ExerciseHistoryFragment.kt
+++ app/src/main/java/ch/hearc/ezworkout/ui/activities/exercise/ExerciseHistoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -18,7 +19,8 @@ import ch.hearc.ezworkout.networking.MainViewModelFactory
 import ch.hearc.ezworkout.networking.model.ExerciseEff
 import ch.hearc.ezworkout.networking.model.TrainingEff
 import ch.hearc.ezworkout.networking.repository.Repository
-import ch.hearc.ezworkout.ui.activities.training.ExerciseContent
+import kotlinx.android.synthetic.main.a_e_exercise_history_fragment.*
+import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
@@ -31,6 +33,7 @@ class ExerciseHistoryFragment : Fragment() {
     private lateinit var myAdapter: ArrayAdapter<String>
     private lateinit var mainViewModel: MainViewModel
     private lateinit var listView: ListView
+    private var loadingData: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,26 +46,189 @@ class ExerciseHistoryFragment : Fragment() {
             false
         )
 
-        // Get ListView object from xml
+        // ListView
         listView = root.findViewById(R.id.list) as ListView
-
-        // Defined Array values to show in ListView
-        val values = mutableListOf(
-            "No data found"
-        )
-
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
+        val values = mutableListOf("")
         myAdapter = ArrayAdapter<String>(
             requireActivity().applicationContext,
             android.R.layout.simple_list_item_1, android.R.id.text1, values
         )
-
-        // Assign adapter to ListView
         listView!!.adapter = myAdapter
+
+        // Prev button
+        val btnPrev: Button = root.findViewById(R.id.prev_button)
+        btnPrev.setOnClickListener {
+            try {
+                if (!loadingData && model.logbookPages.value != null && model.currentLBIndex.value != null) {
+                    loadingData = true
+                    val trainingId = model.trainingId.value
+                    val exerciseId = model.exerciseId.value
+                    val logbookPages = model.logbookPages.value
+
+                    val previousIndex = model.currentLBIndex.value!! - 1
+                    val previousLogbookPage =
+                        if (previousIndex >= 0 && previousIndex < logbookPages!!.size) logbookPages!![previousIndex] else null
+
+                    if (previousLogbookPage != null) {
+                        Log.d("Err", "AAA")
+                        model.currentLBIndex.value = previousIndex
+
+                        mainViewModel.getTrainingEff(Integer(previousLogbookPage.id))
+                        mainViewModel.trainingEffResponse.observe(
+                            viewLifecycleOwner,
+                            Observer { response ->
+                                val effTrainings = response
+                                var effTraining: TrainingEff? = null
+
+                                effTrainings.forEach {
+                                    if (it.trainingId == trainingId) effTraining = it
+                                }
+
+                                if (effTraining != null) {
+                                    Log.d("Err", "BBB")
+
+                                    mainViewModel.getExerciseEff(Integer(effTraining!!.id))
+                                    mainViewModel.exerciseEffResponse.observe(
+                                        viewLifecycleOwner,
+                                        Observer { response ->
+                                            val effExercises = response
+                                            var effExercise: ExerciseEff? = null
+
+                                            effExercises.forEach {
+                                                if (it.exerciseId == exerciseId) effExercise = it
+                                            }
+
+                                            if (effExercise != null) {
+                                                val pattern = Regex("\\d{4}-\\d{2}-\\d{2}")
+                                                val formattedDate =
+                                                    pattern.find(effExercise!!.createdAt!!, 0)
+                                                DateTitle.text = formattedDate?.value ?: "Error"
+
+                                                Log.d("Err", "CCC")
+
+                                                mainViewModel.getSeriesEff(Integer(effExercise!!.id))
+                                                mainViewModel.seriesEffResponse.observe(
+                                                    viewLifecycleOwner,
+                                                    Observer { response ->
+                                                        val effSeries = response
+
+                                                        myAdapter.clear()
+                                                        var i = 1
+                                                        effSeries.forEach {
+                                                            myAdapter.add("Série " + i + " : " + it.weight + "kg - x" + it.rep)
+                                                            i++
+                                                        }
+
+                                                        // Notify adapter
+                                                        myAdapter.notifyDataSetChanged()
+                                                        loadingData = false
+                                                    })
+                                            } else {
+                                                DateTitle.text = "No data"
+                                                loadingData = false
+                                                Log.d("Err", "No effExercise found")
+                                            }
+                                        })
+                                } else {
+                                    DateTitle.text = "No data"
+                                    loadingData = false
+                                    Log.d("Err", "No effTraining found")
+                                }
+                            })
+                    } else {
+                        loadingData = false
+                        Log.d("Err", "No previous lastbookpage found")
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("EXCEPTION", e.toString())
+            }
+        }
+
+        // Next button
+        val btnNext: Button = root.findViewById(R.id.next_button)
+        btnNext.setOnClickListener {
+            try {
+                if (!loadingData && model.logbookPages.value != null && model.currentLBIndex.value != null) {
+                    loadingData = true
+                    val trainingId = model.trainingId.value
+                    val exerciseId = model.exerciseId.value
+                    val logbookPages = model.logbookPages.value
+
+                    val nextIndex = model.currentLBIndex.value!! + 1
+                    val nextLogbookPage =
+                        if (nextIndex >= 0 && nextIndex < logbookPages!!.size) logbookPages!![nextIndex] else null
+
+                    if (nextLogbookPage != null) {
+                        model.currentLBIndex.value = nextIndex
+
+                        mainViewModel.getTrainingEff(Integer(nextLogbookPage.id))
+                        mainViewModel.trainingEffResponse.observe(
+                            viewLifecycleOwner,
+                            Observer { response ->
+                                val effTrainings = response
+                                var effTraining: TrainingEff? = null
+
+                                effTrainings.forEach {
+                                    if (it.trainingId == trainingId) effTraining = it
+                                }
+
+                                if (effTraining != null) {
+                                    mainViewModel.getExerciseEff(Integer(effTraining!!.id))
+                                    mainViewModel.exerciseEffResponse.observe(
+                                        viewLifecycleOwner,
+                                        Observer { response ->
+                                            val effExercises = response
+                                            var effExercise: ExerciseEff? = null
+
+                                            effExercises.forEach {
+                                                if (it.exerciseId == exerciseId) effExercise = it
+                                            }
+
+                                            if (effExercise != null) {
+                                                val pattern = Regex("\\d{4}-\\d{2}-\\d{2}")
+                                                val formattedDate =
+                                                    pattern.find(effExercise!!.createdAt!!, 0)
+                                                DateTitle.text = formattedDate?.value ?: "Error"
+
+                                                mainViewModel.getSeriesEff(Integer(effExercise!!.id))
+                                                mainViewModel.seriesEffResponse.observe(
+                                                    viewLifecycleOwner,
+                                                    Observer { response ->
+                                                        val effSeries = response
+
+                                                        myAdapter.clear()
+                                                        var i = 1
+                                                        effSeries.forEach {
+                                                            myAdapter.add("Série " + i + " : " + it.weight + "kg - x" + it.rep)
+                                                            i++
+                                                        }
+
+                                                        // Notify adapter
+                                                        myAdapter.notifyDataSetChanged()
+                                                        loadingData = false
+                                                    })
+                                            } else {
+                                                DateTitle.text = "No data"
+                                                loadingData = false
+                                                Log.d("Err", "No effExercise found")
+                                            }
+                                        })
+                                } else {
+                                    DateTitle.text = "No data"
+                                    loadingData = false
+                                    Log.d("Err", "No effTraining found")
+                                }
+                            })
+                    } else {
+                        loadingData = false
+                        Log.d("Err", "No next lastbookpage found")
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("EXCEPTION", e.toString())
+            }
+        }
 
         return root
     }
@@ -86,10 +252,16 @@ class ExerciseHistoryFragment : Fragment() {
         mainViewModel.getLogbookPage(Integer(trainingPlanId!!))
         mainViewModel.logbookPageResponse.observe(viewLifecycleOwner, Observer { response ->
             val logbookPages = response
-            val lastLogbookPage = if (logbookPages.isEmpty()) null else logbookPages.last()
+            model.logbookPages.value = logbookPages
 
-            if (lastLogbookPage != null) {
-                mainViewModel.getTrainingEff(Integer(lastLogbookPage.id))
+            val currentLBIndex = if (logbookPages.size < 2) 0 else logbookPages.lastIndex - 1
+            val pageBeforeLastLogbookPage =
+                if (logbookPages.size < 2) null else logbookPages[currentLBIndex]
+
+            if (pageBeforeLastLogbookPage != null) {
+                model.currentLBIndex.value = currentLBIndex
+
+                mainViewModel.getTrainingEff(Integer(pageBeforeLastLogbookPage.id))
                 mainViewModel.trainingEffResponse.observe(viewLifecycleOwner, Observer { response ->
                     val effTrainings = response
                     var effTraining: TrainingEff? = null
@@ -111,6 +283,10 @@ class ExerciseHistoryFragment : Fragment() {
                                 }
 
                                 if (effExercise != null) {
+                                    val pattern = Regex("\\d{4}-\\d{2}-\\d{2}")
+                                    val formattedDate = pattern.find(effExercise!!.createdAt!!, 0)
+                                    DateTitle.text = formattedDate?.value ?: "Error"
+
                                     mainViewModel.getSeriesEff(Integer(effExercise!!.id))
                                     mainViewModel.seriesEffResponse.observe(
                                         viewLifecycleOwner,
@@ -118,7 +294,6 @@ class ExerciseHistoryFragment : Fragment() {
                                             val effSeries = response
 
                                             myAdapter.clear()
-
                                             var i = 1
                                             effSeries.forEach {
                                                 myAdapter.add("Série " + i + " : " + it.weight + "kg - x" + it.rep)
@@ -127,12 +302,25 @@ class ExerciseHistoryFragment : Fragment() {
 
                                             // Notify adapter
                                             myAdapter.notifyDataSetChanged()
+                                            loadingData = false
                                         })
-                                } else Log.d("Err", "No effExercise found")
+                                } else {
+                                    DateTitle.text = "No data"
+                                    loadingData = false
+                                    Log.d("Err", "No effExercise found")
+                                }
                             })
-                    } else Log.d("Err", "No effTraining found")
+                    } else {
+                        DateTitle.text = "No data"
+                        loadingData = false
+                        Log.d("Err", "No effTraining found")
+                    }
                 })
-            } else Log.d("Err", "No lastbookpage found")
+            } else {
+                DateTitle.text = "No data"
+                loadingData = false
+                Log.d("Err", "No lastbookpage found")
+            }
         })
     }
 
--- app/src/main/java/ch/hearc/ezworkout/ui/activities/exercise/ExerciseViewModel.kt
+++ app/src/main/java/ch/hearc/ezworkout/ui/activities/exercise/ExerciseViewModel.kt
@@ -2,11 +2,15 @@ package ch.hearc.ezworkout.ui.activities.exercise
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ch.hearc.ezworkout.networking.model.LogbookPage
 
 class ExerciseViewModel : ViewModel() {
 
     val trainingPlanId = MutableLiveData<Int>()
     val trainingId = MutableLiveData<Int>()
     val exerciseId = MutableLiveData<Int>()
+
+    val logbookPages = MutableLiveData<List<LogbookPage>>()
+    val currentLBIndex = MutableLiveData<Int>()
 
 }
